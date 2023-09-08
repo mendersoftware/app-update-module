b09574468d7169d103060a969f3a4a0cbf6e2a85
Peter Grzybowski <peter@northern.tech>
2023-09-08 11:14:39 +0200

    test: local artifact
    Ticket: MEN-6194
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- acceptance-tests/scenarios.d/00.k8s.basic.non.delta.successful.sh
+++ acceptance-tests/scenarios.d/00.k8s.basic.non.delta.successful.sh
@@ -25,6 +25,7 @@ function test_phase_setup() {
     mender-artifact --version || return 1
     k3d cluster create acceptance-tests
     kubectl get nodes
+    mv -f /usr/bin/ctr /usr/bin/ctr-prev
     ln -sf /bin/true /bin/ctr
     ln -sf /bin/true /usr/bin/ctr
     return 0
@@ -37,19 +38,24 @@ function test_phase_run() {
     local image1
     local -r timeout_s=32
 
+    mv -f /var/lib/mender/device_type /var/lib/mender/device_type-prev
+    echo -ne "dev0" > /var/lib/mender/device_type
     echo "entering run phase"
-    image1=docker.io/library/postgres:15.4
-    "${GENERATOR:-./gen/app-gen}" \
-        --artifact-name "${artifact_name}" \
-        --device-type "$(cat /var/lib/mender/device_type | sed -e 's/^.*=//')" \
-        --output-path "${artifact_file}" \
-        --image "${image1}" \
-        --platform linux/amd64 \
-        --orchestrator k8s \
-        --k8s-namespace acceptance-tests \
-        --manifests-dir acceptance-tests/data/k8s-manifests-1 \
-        --application-name myapp0 || return 1
+    artifact_file="acceptance-tests/data/k8s-a0.mender"
+#    image1=docker.io/library/memcached:1.6.18-alpine
+#    image1=docker.io/library/postgres:15.4
+#    "${GENERATOR:-./gen/app-gen}" \
+#        --artifact-name "${artifact_name}" \
+#        --device-type "$(cat /var/lib/mender/device_type | sed -e 's/^.*=//')" \
+#        --output-path "${artifact_file}" \
+#        --image "${image1}" \
+#        --platform linux/amd64 \
+#        --orchestrator k8s \
+#        --k8s-namespace acceptance-tests \
+#        --manifests-dir acceptance-tests/data/k8s-manifests-1 \
+#        --application-name myapp0 || return 1
 
+    kubectl get pods --namespace acceptance-tests
     mender install "$artifact_file" || return 2
     sleep "${timeout_s}"
     kubectl get pods --namespace acceptance-tests
@@ -58,10 +64,14 @@ function test_phase_run() {
 
 function test_failed_hook_phase_run() {
     echo "test run failed."
+    mv -f /usr/bin/ctr-prev /usr/bin/ctr
+    mv -f /var/lib/mender/device_type-prev /var/lib/mender/device_type
     exit 1
 }
 
 function test_failed_hook_phase_setup() {
-    echo "tests setup failed."
-    exit 1
+   echo "tests setup failed."
+   mv -f /usr/bin/ctr-prev /usr/bin/ctr
+    mv -f /var/lib/mender/device_type-prev /var/lib/mender/device_type
+   exit 1
 }
